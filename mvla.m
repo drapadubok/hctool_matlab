6580204cfba32a92811f7d609f37f332b8ec362c
drapadubok <dmi.smirnov07@gmail.com>
2015-11-16 12:25:50 +0200

    fixed cells

--- mvla.m
+++ mvla.m
@@ -51,44 +51,67 @@ for r = 1:length(preds) % for each scale
     end
     info(r,1) = mean(log(probrightclass));
 end
-%% find best scale, based on mlpp
-bestlambdaidx = find(info==max(info)); % keep the one which has better predictive rate
+%% find best scale, based on rate
+bestlambdaidx = find(rate==max(rate)); 
 if length(bestlambdaidx) > 1 % if a draw
-    bestlambdaidx = find(rate==max(rate)); % best based on accuracy
+    bestlambdaidx = find(info==max(info)); % best based on accuracy
 end
 if length(bestlambdaidx) > 1
     bestlambdaidx = bestlambdaidx(1); % if still a draw, take just the first one
 end
+% 
+% bestlambdaidx = find(info==max(info)); % keep the one which has better predictive rate
+% if length(bestlambdaidx) > 1 % if a draw
+%     bestlambdaidx = find(rate==max(rate)); % best based on accuracy
+% end
+% if length(bestlambdaidx) > 1
+%     bestlambdaidx = bestlambdaidx(1); % if still a draw, take just the first one
+% end
 % save outputs at this stage
-retval.preds = preds{bestlambdaidx};
-retval.accuracy = max(rate);
+retval.preds = preds;
+retval.accuracy = rate(bestlambdaidx);
 retval.lambda = lambda(bestlambdaidx);
 retval.cls = objs{bestlambdaidx};
 retval.mlpp = info(bestlambdaidx);
 retval.lambdamlpp = info;
 retval.lambdarate = rate;
+retval.lambdaall = lambda;
 %% For pictures
 % Run model with all the data, no CV
 % According to toy example, higher probabilities work towards class 1 vs class 2
 bestlambda = lambda(bestlambdaidx);
 for c = 1:cfg.ntask
     clear f
-    f = ft_mv_blogreg('scale', bestlambda);
+%     f = ft_mv_blogreg('scale', bestlambda);
+    f = ft_mv_blogreg_edit2('scale', lambda); % to get maps for all lambdas, that is how we will get maps anyway
+    clear objs    
     % take all data
     x = data;
     % make proper labels
     y = zeros(size(labels));
     y(labels==c) = 1;
     y(labels~=c) = 2;
-    f = f.train(x,y);
-    probs{c} = 1-normcdf(0,f.Gauss.m(1:end-1),sqrt(f.Gauss.diagC(1:end-1))); % impmap, calculate without crossval
-    weights{c} = f.Gauss.m(1:end-1);
+%     f = f.train(x,y);
+    [~, objs] = f.train(x,y);
+    %% if some lambda failed, cleanup
+    emptyCells = find(cellfun(@isempty,objs));
+    if ~isempty(emptyCells)
+        for j = length(emptyCells):-1:1
+            lambda(emptyCells(j)) = [];
+            objs(emptyCells(j)) = [];
+        end
+    end
+%     probs{c} = 1-normcdf(0,f.Gauss.m(1:end-1),sqrt(f.Gauss.diagC(1:end-1))); % impmap, calculate without crossval
+%     weights{c} = f.Gauss.m(1:end-1);
+    for l = 1:length(lambda)
+        probs{c,l} = 1-normcdf(0,objs{l}.Gauss.m(1:end-1),sqrt(objs{l}.Gauss.diagC(1:end-1))); % impmap, calculate without crossval
+        weights{c,l} = objs{l}.Gauss.m(1:end-1);
+    end    
 end
 % save outputs for picture
 retval.probs = probs;
 retval.weights = weights;
 save(sprintf('%s/within/%s/%s/results.mat',cfg.dataroot,name,cfg.subject),'retval','-v7.3');
-
 
 % %% Execute with the best scale, collect accuracy
 % clear preds
@@ -122,7 +145,7 @@ save(sprintf('%s/within/%s/%s/results.mat',cfg.dataroot,name,cfg.subject),'retva
 %     f = objs{r};
 %     probs{r} = 1-normcdf(0,f.Gauss.m(1:end-1),sqrt(f.Gauss.diagC(1:end-1))); % impmap, calculate without crossval
 % end
-
+exit;
 
 
 
